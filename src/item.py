990a557cfcd9df704107e6876826deb82a026b82
Edudeiko <evgeniy.dudeyko@gmail.com>
2020-09-08 23:58:03 -0400

    day 1

---
+++ src/item.py
@@ -0,0 +1,11 @@
+class Item:
+    '''The item should have `name` and `description` attributes
+    '''
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    
+
+    def __str__(self):
+        return(self.name)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,16 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    '''Rooms in the game
+
+    name and description attributes,
+    The room should also have `n_to`, `s_to`, `e_to`, and `w_to` attributes
+    '''
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
